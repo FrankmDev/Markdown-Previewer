08c892fd67dbdb3b972a213bf762a124e193b7fd
FrankmDev <frankmmarindev@gmail.com>
2023-11-29 10:22:01 +0100

    MarkDown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
+import Markdown from "react-markdown";
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Markdown>This is *italic* text and **bold** text...</Markdown>
     </>
   );
 }
