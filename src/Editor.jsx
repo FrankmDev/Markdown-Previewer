cc1e0280e66c457a7d459ba0a907bb2528c625ae
FrankmDev <frankmmarindev@gmail.com>
2023-11-30 19:37:10 +0100

    Markdown Previewer

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,6 +1,18 @@
+/* eslint-disable react/prop-types */
 import "./editor.css";
-const Editor = () => {
-  return <textarea name="editor" id="editor" cols="50" rows="30"></textarea>;
+const Editor = ({ initialText, onChange }) => {
+  return (
+    <textarea
+      name="editor"
+      id="editor"
+      cols="50"
+      rows="30"
+      value={initialText}
+      onChange={onChange}
+    >
+      {initialText}
+    </textarea>
+  );
 };
 
 export default Editor;
--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,6 +1,12 @@
+/* eslint-disable react/prop-types */
 import "./preview.css";
-const Preview = () => {
-  return <section id="preview"></section>;
+import Markdown from "react-markdown";
+const Preview = ({ text }) => {
+  return (
+    <section id="preview">
+      <Markdown>{text}</Markdown>
+    </section>
+  );
 };
 
 export default Preview;
